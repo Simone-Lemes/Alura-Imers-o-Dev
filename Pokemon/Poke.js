dbb12b1031b463c3eb71d49d1256abd23c158498
Simone-Lemes <simonelemesacs@gmail.com>
2021-03-23 14:45:09 -0300

    Pokemon
    projeto pronto

---
+++ Pokemon/Poke.js
@@ -0,0 +1,22 @@
+var identificacao = parseInt(prompt("Qual o nome desse Pokémon ? Digite: \n1 para Charmeleon, \n2 para Charizard, \n3 para Charmander"))
+var nome = "Parabéns!!! Esse é o Charmander"
+
+if (identificacao == 3) {
+    var resultado = nome
+    //mostrar o h2 do html na calculadora
+    document.write("<h2>" + nome + "</h2>")    
+} else {1
+    document.write("<h2>Opção inválida</h2>")
+}
+ 
+//escrevendo na tela = document.write()
+//concatenação = juntar palavras ("palavra" + "variável")
+// == (comparação) é diferente do = que usamos para fazer atribuição
+
+//if = se
+//else = senão
+//else if = se não se
+
+
+
+
