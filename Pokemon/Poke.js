4356d483e313ce38408b6c6cee9ccc9e1bdc5d90
Simone-Lemes <simonelemesacs@gmail.com>
2021-03-23 21:14:26 -0300

    .

--- Pokemon/Poke.js
+++ Pokemon/Poke.js
@@ -1,3 +1,6 @@
+var nomePoke = "Escolha um número e diga qual o nome do Pokémon";
+alert (nomePoke);
+
 var identificacao = parseInt(prompt("Qual o nome desse Pokémon ? Digite: \n1 para Charmeleon, \n2 para Charizard, \n3 para Charmander"))
 var nome = "Parabéns!!! Esse é o Charmander"
 
@@ -5,10 +8,10 @@ if (identificacao == 3) {
     var resultado = nome
     //mostrar o h2 do html na calculadora
     document.write("<h2>" + nome + "</h2>")    
-} else {1
+} else {
     document.write("<h2>Que pena o nome dele não é esse, \ntente novamente</h2>")
 }
- 
+
 //escrevendo na tela = document.write()
 //concatenação = juntar palavras ("palavra" + "variável")
 // == (comparação) é diferente do = que usamos para fazer atribuição
@@ -16,7 +19,6 @@ if (identificacao == 3) {
 //if = se
 //else = senão
 //else if = se não se
-
 
 
 
