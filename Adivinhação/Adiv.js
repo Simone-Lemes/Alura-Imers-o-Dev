caa27e49f25ee868aa4d73414bdad045b57a87e7
Simone-Lemes <simonelemesacs@gmail.com>
2021-03-24 21:04:07 -0300

    Adivinhação e Mentalista

---
+++ Adivinhação/Adiv.js
@@ -0,0 +1,26 @@
+var numeroAleatorio = parseInt(Math.random()* 10)
+var numeroTentativas = 3;
+
+            while (numeroTentativas > 0) {
+                var numeroChute = parseInt(prompt("Digite um número entre 0 e 10"));
+
+                numeroTentativas++
+                if (numeroAleatorio == numeroChute) {
+                    alert(" Acertou ");
+                break;
+               
+               } else if (numeroChute > numeroAleatorio) {
+                alert (" O número secreto é menor ");
+                continue;
+               
+               } else if (numeroChute < numeroAleatorio) {
+                alert (" O número secreto é maior ");
+                continue;
+            }
+        }
+            
+        
+                   
+                
+            
+            
---
+++ Mentalista/Mentalista.js
@@ -0,0 +1,34 @@
+// Variável de número aleatório
+var numeroSecreto = parseInt(Math.random()* 10)
+
+//alert (numeroSecreto) (Mostra o número secreto escolhido pelo random)
+// var numeroSecreto = 3; (Variável de Número pre definido)
+
+var tentativas = 3
+
+while (tentativas > 0) {
+var chute = parseInt(prompt("Digite um número entre 0 e 10"));
+
+if (numeroSecreto == chute) {
+    alert(" Acertou ");
+    break;
+} else if (chute > numeroSecreto) {
+ alert (" O número secreto é menor ");
+ tentativas = tentativas -1;
+} else if (chute < numeroSecreto) {
+alert (" O número secreto é maior ");
+tentativas = tentativas -1;
+  }
+}
+if (chute != numeroSecreto) {
+    alert (" Suas tentativas acabaram. O número secreto era " + numeroSecreto);
+}
+
+
+
+            
+        
+                   
+                
+            
+            
