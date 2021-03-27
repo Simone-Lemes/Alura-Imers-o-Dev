64b64d51cc7fbb0565daaee801baf89dcba743c7
Simone-Lemes <simonelemesacs@gmail.com>
2021-03-27 00:21:35 -0300

    css

--- AluraFlix/Flix.js
+++ AluraFlix/Flix.js
@@ -7,10 +7,10 @@ for (var i = 0; i < listaFilmes.length; i++) {
 }
 
 //inserir imagens no Array
-//var tituloFilmes = ["Toy Story", "https://bit.ly/3lQibYB","https://bit.ly/3waLdXI", "https://bit.ly/3tW0zNC", "https://bit.ly/3d253vq", "https://bit.ly/3lQSCGN", ""];
+//var tituloFilmes = ["https://cdn.pixabay.com/photo/2012/04/15/19/10/rectangle-34969_960_720.png", "https://bit.ly/3lQibYB","https://bit.ly/3waLdXI", "https://bit.ly/3tW0zNC", "https://bit.ly/3d253vq", "https://bit.ly/3lQSCGN", ""];
 
 //Mostra a imagem do Array em HTML
 //for (var f = 0; f < tituloFilmes.length; f++) {
- //   document.write("<img src =" + tituloFilmes [f] + ">")
+//   document.write("<img src =" + tituloFilmes [f] + ">")
 //}
 
