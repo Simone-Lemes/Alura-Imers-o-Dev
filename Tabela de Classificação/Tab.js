7d876f57c0b04b536d944a18996a8bdd03d22d0b
Simone-Lemes <simonelemesacs@gmail.com>
2021-03-29 09:39:41 -0300

    Tabela

---
+++ Tabela de Classificação/Tab.js
@@ -0,0 +1,19 @@
+var paulo = {
+    nome: "Paulo",
+    vitorias: 2,
+    empates: 5,
+    derrotas: 1,
+    pontos: 11,
+}
+
+var rafa = {
+    nome: "Rafa",
+    vitorias: 3,
+    empates: 5,
+    derrotas: 2,
+    pontos: 0,
+}
+
+//exibe as vitorias com o . depois da variável poderia ser qualquer outra informação
+console.log(paulo.vitorias);
+console.log(rafa.derrotas);
